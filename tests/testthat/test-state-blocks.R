2463d6dcbf6dbcc744067bd26e1699ca8481eb0f
dgkf <18220321+dgkf@users.noreply.github.com>
2023-02-02 17:32:42 -0500

    shortening fixture paths

--- tests/testthat/test-state-blocks.R
+++ tests/testthat/test-state-blocks.R
@@ -2,7 +2,7 @@ test_that("roxygen_blocks can be scraped for topenv namespace", {
   expect_silent(clear_state())
 
   tp <- normalizePath(testthat::test_path())
-  p <- file.path(tp, "fixtures", "pkg_desc_config")
+  p <- file.path(tp, "fix", "pkgDESC")
   expect_true({
     blocks <- roxygen_blocks(path = p)
     TRUE
@@ -13,7 +13,7 @@ test_that("roxygen_blocks can be scraped for topenv namespace", {
 
 test_that("associated_block can find the block containing a tag", {
   tp <- normalizePath(testthat::test_path())
-  f <- file.path(tp, "fixtures", "pkg_desc_config", "R", "fn.R")
+  f <- file.path(tp, "fix", "pkgDESC", "R", "fn.R")
   expect_true(file.exists(f))
   expect_s3_class(associated_block(f, 3), "roxy_block")
   expect_null(associated_block(f, 1e5))
